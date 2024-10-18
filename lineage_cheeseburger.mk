7b18de1f4ef061a2a5a7be537a1330ce3bb0e1fc
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    cheeseburger: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_cheeseburger.mk
+++ lineage_cheeseburger.mk
@@ -22,17 +22,13 @@ PRODUCT_MODEL := ONEPLUS A5000
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-PRODUCT_SYSTEM_DEVICE := OnePlus5
-PRODUCT_SYSTEM_NAME := OnePlus5
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus5 \
-    TARGET_PRODUCT=OnePlus5
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus5-user 10 QKQ1.191014.012 2010292059 release-keys"
-
-BUILD_FINGERPRINT := OnePlus/OnePlus5/OnePlus5:10/QKQ1.191014.012/2010292059:user/release-keys
+    BuildDesc="OnePlus5-user 10 QKQ1.191014.012 2010292059 release-keys" \
+    BuildFingerprint=OnePlus/OnePlus5/OnePlus5:10/QKQ1.191014.012/2010292059:user/release-keys \
+    DeviceName=OnePlus5 \
+    DeviceProduct=OnePlus5 \
+    SystemDevice=OnePlus5 \
+    SystemName=OnePlus5
 
 TARGET_VENDOR := oneplus
 
